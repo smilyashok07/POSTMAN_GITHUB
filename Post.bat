c578d918334493e1001c8d14b2144f1679be4b24
smilyashok07 <a4uashu1@gmail.com>
2016-06-28 18:31:13 +0530

    ff

--- Post.bat
+++ Post.bat
@@ -1,3 +1,3 @@
-C:
-cd C:\Users\venkataapparao.chera\AppData\Roaming\npm\node_modules\newman\bin
-node newman -c tests.json -e env.json -g globals.json -H Output.html 
+D:
+cd D:\Postman\POSTMAN_GITHUB
+newman -c tests.json -e env.json -g globals.json -H Output.html 
