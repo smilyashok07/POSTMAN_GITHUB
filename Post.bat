4456655d3872b3609f6e1d90c75d471de13e3cf9
smilyashok07 <a4uashu1@gmail.com>
2016-06-28 19:03:01 +0530

    sfsfsd

--- Post.bat
+++ Post.bat
@@ -1,3 +1,1 @@
-d:
-cd D:\Postman\POSTMAN_GITHUB
-node C:/Users/venkataapparao.chera/AppData/Roaming/npm/node_modules/newman/bin/newman -c tests.json -e env.json -g globals.json -H Output.html 
+node C:/Users/venkataapparao.chera/AppData/Roaming/npm/node_modules/newman/bin/newman -c tests.json -e env.json -g globals.json -H output.html 
