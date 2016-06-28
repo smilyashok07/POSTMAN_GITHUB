f48eaadabe53e2044ef654bf8a20d117c08fdc63
smilyashok07 <a4uashu1@gmail.com>
2016-06-28 18:49:03 +0530

    update
    update

--- Post.bat
+++ Post.bat
@@ -1,3 +1,3 @@
-D:
+d:
 cd D:\Postman\POSTMAN_GITHUB
-newman -c tests.json -e env.json -g globals.json -H Output.html 
+node C:/Users/venkataapparao.chera/AppData/Roaming/npm/node_modules/newman/bin/newman -c tests.json -e env.json -g globals.json -H Output.html 
