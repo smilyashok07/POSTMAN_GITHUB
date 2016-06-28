35e5c72a8cb1dab951786df5a9de9400b7b1f23d
smilyashok07 <a4uashu1@gmail.com>
2016-06-28 18:22:32 +0530

    daqe
    ewq

--- Post.bat
+++ Post.bat
@@ -1,3 +1,3 @@
 C:
-cd C:/Users/venkataapparao.chera/AppData/Roaming/npm/node_modules/newman/bin/
-node newman -c tests.json -e env.json -g globals.json -H output.html 
+cd C:\Users\venkataapparao.chera\AppData\Roaming\npm\node_modules\newman\bin
+node newman -c tests.json -e env.json -g globals.json -H Output.html 
